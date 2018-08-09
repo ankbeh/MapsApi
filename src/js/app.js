b1ca722527e761ae01fddd386e59bd903e955897
ankbeh <ankitbuzzid@gmail.com>
2018-08-09 16:07:40 +0530

    Allow Location Searching

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,18 @@
 var map, watchId, userPin;
+//witing for form submit event
+document.getElementById("query").addEventListener("submit", MapSearch);
 
 function GetMap()
 {
 	map = new Microsoft.Maps.Map('#myMap', {
 		credentials: bing.key
 	});
-	StartTracking();
+	//StartTracking();//On Load start tracking
 }
 
 function StartTracking() {
+	map.entities.clear();
+	setTimeout('', 2000);
 	//Add a pushpin to show the user's location.
 	userPin = new Microsoft.Maps.Pushpin(map.getCenter(), { visible: false });
 	map.entities.push(userPin);
@@ -28,5 +32,32 @@ function UsersLocationUpdated(position) {
 	userPin.setOptions({ visible: true });
 
 	//Center the map on the user's location.
-	map.setView({ center: loc });
+	map.setView({ center: loc, zoom:19 });
+}
+
+function StopTracking() {
+		// Cancel the geolocation updates.
+		navigator.geolocation.clearWatch(watchId);
+
+		//Remove the user pushpin.
+		map.entities.clear();
+}
+
+function MapSearch() {
+	StopTracking();//Stop Tracking so can search for position
+	var SearchLocation = document.getElementById('SearchLocation').value;
+	document.getElementById("query").reset();
+  Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+    var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+    var requestOptions = {
+      bounds: map.getBounds(),
+      where: SearchLocation,
+      callback: function (answer, userData) {
+        map.setView({ bounds: answer.results[0].bestView });
+        map.entities.push(new Microsoft.Maps.Pushpin(answer.results[0].location));
+				//console.log(answer.results[0].location.coords.latitude+" "+answer.results[0].location.lcoords.longitude);
+        }
+    };
+    searchManager.geocode(requestOptions);
+  });
 }
